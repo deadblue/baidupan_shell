b63c078cdfb242f8dd35b8cd75757295b87d23d4
deadblue <9deadblue@gmail.com>
2014-07-09 14:59:04 +0800

    实现lcd指令

--- baidupan/command/lcd_command.py
+++ baidupan/command/lcd_command.py
@@ -4,6 +4,7 @@ Created on 2014/07/07
 
 @author: deadblue
 '''
+
 from baidupan import context
 from baidupan.command import Command
 import os
@@ -19,6 +20,28 @@ class LocalChangeDirectoryCommand(Command):
         else:
             lwd = '%s%s' % (lwd, arg)
             lwd = os.path.abspath(lwd)
-            if not lwd.endswith('/'):
-                lwd += '/'
-        context.set_lwd(lwd)
+        if not lwd.endswith('/'):
+            lwd += '/'
+        # 设置本地工作目录
+        if os.path.exists(lwd):
+            context.set_lwd(lwd)
+        else:
+            print 'No such path!'
+    def get_completer_words(self, prefix):
+        # 获取要寻址的路径
+        if prefix.startswith('/'):
+            lwd = prefix
+        else:
+            if prefix is None: prefix = ''
+            lwd = '%s%s' % (context.get_lwd(), prefix)
+        # 处理路径中的相对路径
+        if lwd.endswith('/'):
+            lwd = os.path.abspath(lwd) + '/'
+        else:
+            lwd = os.path.abspath(lwd)
+        # 拆分出父目录和子目录前缀
+        parent_dir, sub_prefix = os.path.split(lwd)
+        # 列出父目录下所有目录
+        sub_dirs = os.listdir(parent_dir)
+        # 列出所有符合前缀的子目录
+        return filter(lambda x:x.startswith(sub_prefix), sub_dirs)
