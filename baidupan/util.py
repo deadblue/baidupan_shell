aee05705b8afee169c54f16a928e5dc80d009a63
deadblue <9deadblue@gmail.com>
2014-07-09 11:25:02 +0800

    增加日期时间和文件大小的格式化函数

--- baidupan/util.py
+++ baidupan/util.py
@@ -11,6 +11,22 @@ import time
 def timstamp():
     return int(time.time() * 1000)
 
+def format_time(seconds):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(seconds))
+
+def format_size(size):
+    if size > 1073741824:
+        size /= 1073741824.0
+        return '%.2f GB' % size
+    elif size > 1048576:
+        size /= 1048576.0
+        return '%.2f MB' % size
+    elif size > 1024:
+        size /= 1024.0
+        return '%.2f KB' % size
+    else:
+        return str(size)
+
 def random_hex_str(length=6):
     source = '0123456789abcdef'
     return random_str(source, length)
