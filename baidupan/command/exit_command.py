248eff3e1aff71c118e6267852902d3c99cd23b0
deadblue <9deadblue@gmail.com>
2014-07-09 11:19:13 +0800

    新增删除命令

---
+++ baidupan/command/exit_command.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014/07/09
+
+@author: deadblue
+'''
+
+from baidupan.command import Command
+from baidupan import context
+
+class ExitCommand(Command):
+    def __init__(self):
+        Command.__init__(self, 'exit', False)
+    def execute(self, arg=None):
+        context.alive = False
