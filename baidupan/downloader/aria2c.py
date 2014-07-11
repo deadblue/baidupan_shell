20ec866f26c1be86978a84aa2a52e695a5b2412d
deadblue <9deadblue@gmail.com>
2014-07-11 15:52:54 +0800

    实现wget和aria2c下载，wget暂时有点问题……

--- baidupan/downloader/aria2c.py
+++ baidupan/downloader/aria2c.py
@@ -4,3 +4,29 @@ Created on 2014/07/11
 
 @author: deadblue
 '''
+
+from baidupan import context
+import subprocess
+import os
+
+def download(file_req, save_path):
+    cmd = ['aria2c']
+    # user-agent
+    ua = file_req.get_header('User-Agent')
+    if ua: cmd.append('--user-agent=%s' % ua)
+    # referer
+    ref = file_req.get_header('Referer')
+    if ref: cmd.append('--referer=%s"' % ref)
+    # cookie
+    cmd.append('--load-cookies')
+    cmd.append(context.cookie_file)
+    # save path
+    save_dir, save_name = os.path.split(save_path)
+    cmd.append('--dir')
+    cmd.append(save_dir)
+    cmd.append('--out')
+    cmd.append(save_name)
+    # download url
+    cmd.append(file_req.get_full_url())
+    # run it
+    subprocess.call(cmd)
--- baidupan/downloader/wget.py
+++ baidupan/downloader/wget.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 '''
+wget下载器，传cookie有问题，待修正
 Created on 2014/07/11
 
 @author: deadblue
 '''
+
+import subprocess
+from baidupan import context
+
+def download(file_req, save_path):
+    cmd = ['wget']
+    # user-agent
+    ua = file_req.get_header('User-Agent')
+    if ua: cmd.append('--user-agent="%s"' % ua)
+    # referer
+    ref = file_req.get_header('Referer')
+    if ref: cmd.append('--referer="%s"' % ref)
+    # cookie
+    cmd.append('--load-cookies')
+    cmd.append(context.cookie_file)
+    # download url
+    cmd.append(file_req.get_full_url())
+    # save_path
+    cmd.append('-O')
+    cmd.append(save_path)
+    print cmd
+    # execute it
+    subprocess.call(cmd)
