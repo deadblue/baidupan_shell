9d26bed87f34f7fcfe9144ae8d7811c00d455003
deadblue <9deadblue@gmail.com>
2014-07-11 12:18:04 +0800

    将播放视频功能从client中独立出来，client只提供下载需要的信息，播放或下载由命令处理

--- baidupan/command/play.py
+++ baidupan/command/play.py
@@ -5,12 +5,14 @@ Created on 2014/07/10
 @author: deadblue
 '''
 
-from baidupan.command import Command
 from baidupan import context
+from baidupan.command import Command
+from baidupan.player import mplayer
 
 class PlayCommand(Command):
     def __init__(self):
         Command.__init__(self, 'play', True)
     def execute(self, file_id):
         file_id = int(file_id)
-        context.client.play(file_id)
+        video_req = context.client.get_download_request(file_id)
+        mplayer.play(video_req, 848)
---
+++ baidupan/player/mplayer.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014/07/11
+
+@author: deadblue
+'''
+
+from baidupan import context
+import subprocess
+
+def play(video_req, zoom=None):
+    '''
+    播放视频
+    @param video_req: 视频请求
+    @param zoom: 缩放设置
+    '''
+    cmd = ['mplayer']
+    # 缓冲区大小
+    cmd.append('-cache')
+    cmd.append('8192')
+    # 缩放参数的处理
+    if zoom:
+        if type(zoom) is str and zoom == 'fs':
+            cmd.append('-fs')
+        elif type(zoom) is int:
+            cmd.append('-xy')
+            cmd.append(str(zoom))
+        elif type(zoom) is tuple:
+            cmd.append('-x')
+            cmd.append(str(zoom[0]))
+            cmd.append('-y')
+            cmd.append(str(zoom[1]))
+    # user-agent
+    ua = video_req.get_header('User-Agent')
+    if ua:
+        cmd.append('-user-agent')
+        cmd.append(ua)
+    # referer
+    ref = video_req.get_header('Referer')
+    if ref:
+        cmd.append('-referrer')
+        cmd.append(ref)
+    # cookie
+    cmd.append('-cookies')
+    cmd.append('-cookies-file')
+    cmd.append(context.cookie_file)
+    # 视频地址
+    cmd.append(video_req.get_full_url())
+    # 执行命令
+    subprocess.call(cmd)
