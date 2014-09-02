19fbf5ac2c9d6d169283953fdcc9e715cefcef6e
deadblue <9deadblue@gmail.com>
2014-09-02 18:10:07 +0800

    支持多文件上传

--- baidupan/command/push.py
+++ baidupan/command/push.py
@@ -10,12 +10,25 @@ from baidupan.command import Command, InvalidArgumentException
 import os
 
 class PushCommand(Command):
+
     def __init__(self):
         Command.__init__(self, 'push', True)
-    def execute(self, arg=None):
-        if arg is None: raise InvalidArgumentException()
-        upload_file = arg if arg.startswith('/') else '%s%s' % (context.get_lwd(), arg)
-        if os.path.exists(upload_file) and os.path.isfile(upload_file):
-            context.client.upload_curl(context.get_rwd(), upload_file)
+
+    def execute(self, args=None):
+        if args is None:
+            print 'nothing to push'
+            return
+        if type(args) is str:
+            self.upload_one_file(args)
         else:
-            raise InvalidArgumentException()
+            for arg in args:
+                self.upload_one_file(arg)
+
+    def upload_one_file(self, fn):
+        upload_file = fn if fn.startswith('/') else '%s%s' % (context.get_lwd(), fn)
+        if os.path.exists(upload_file) and os.path.isfile(upload_file):
+            print 'upload file: %s ...' % upload_file
+            result = context.client.upload_curl(context.get_rwd(), upload_file)
+            print 'file saved to: %s !' % result.get('path')
+        else:
+            print 'no such file!'
