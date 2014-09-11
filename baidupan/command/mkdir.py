e40826b3bc9e885c3249f66979f24c919b164961
deadblue <9deadblue@gmail.com>
2014-09-11 17:39:33 +0800

    使用remote_tree判断要创建的目录是否存在

--- baidupan/command/mkdir.py
+++ baidupan/command/mkdir.py
@@ -11,16 +11,15 @@ _logger = logging.getLogger('mkdir')
 class CreateDirectoryCommand(Command):
     def __init__(self):
         Command.__init__(self, 'mkdir', True)
+
     def execute(self, args):
         if len(args) == 0:
             print 'nothing happened...'
         else:
             dir_name = args[0]
-            cache_dirs = context.get_dir_from_cache(context.get_rwd())
-            # 判断目录是否已存在
-            if dir_name in map(lambda x:x['server_filename'], cache_dirs):
-                _logger.error('target dir existed!')
+            dir_path = dir_name if dir_name.startswith('/') else '%s%s' % (context.get_rwd(), dir_name)
+            if context.remote_tree.dir_exists(dir_path):
+                print 'dir:%s existed!' % dir_path
             else:
-                dir_path = '%s%s' % (context.get_rwd(), dir_name)
                 _logger.debug('create remote dir: %s' % dir_path)
                 context.client.create_dir(dir_path)
