21a0b4cff31d0273f177747edb15d01366a1a390
deadblue <9deadblue@gmail.com>
2014-09-11 17:38:58 +0800

    增加判断远程目录是否存在的逻辑，实现自动完成。自动完成暂时还有些bug……

--- baidupan/command/cd.py
+++ baidupan/command/cd.py
@@ -5,9 +5,8 @@ Created on 2014/07/07
 @author: deadblue
 '''
 
-from baidupan import context, path
+from baidupan import context, tree, util
 from baidupan.command import Command
-import os
 
 class ChangeDirectoryCommand(Command):
     def __init__(self):
@@ -15,38 +14,24 @@ class ChangeDirectoryCommand(Command):
 
     def execute(self, args):
         target_dir = args[0] if len(args) > 0 else '/'
-        rwd = context.get_rwd()
         if target_dir.startswith('/'):
             rwd = target_dir
         else:
-            rwd = '%s%s' % (rwd, target_dir)
-            rwd = path.remote_abspath(rwd)
+            rwd = '%s%s' % (context.get_rwd(), target_dir)
+        rwd = tree.remote_abspath(rwd)
         if not rwd.endswith('/'): rwd += '/'
-        context.set_rwd(rwd)
+        if context.remote_tree.dir_exists(rwd):
+            context.set_rwd(rwd)
+        else:
+            print 'no such dir!'
 
     def get_completer_words(self, prefix):
-        '''
-        TODO 自动完成存在问题，推测与unicode有关
-        '''
-        # 获取要寻址的路径
         if prefix.startswith('/'):
-            rwd = prefix
+            target_path = prefix
         else:
-            if prefix is None: prefix = ''
-            rwd = '%s%s' % (context.get_rwd(), prefix)
-        # 处理路径中的相对路径
-        if rwd.endswith('/'):
-            lwd = os.path.abspath(rwd) + '/'
-        else:
-            lwd = os.path.abspath(rwd)
-        # 拆分出父目录和子目录前缀
-        parent_dir, sub_prefix = os.path.split(lwd)
-        # 从缓存中获取父目录下的所有子目录
-        if not context.has_dir_cache(parent_dir):
-            result = context.client.list(parent_dir)
-            context.cache_file_list(parent_dir, result['list'])
-        sub_dirs = context.get_dir_from_cache(parent_dir)
-        # 只保留名称
-        sub_dirs = map(lambda x:x['server_filename'], sub_dirs)
-        # 列出所有符合前缀的子目录
-        return filter(lambda x:unicode(x).startswith(unicode(sub_prefix)), sub_dirs)
+            target_path = '%s%s' % (context.get_rwd(), prefix)
+        target_path = tree.remote_abspath(target_path)
+        parent_dir, name_prefix = tree.remote_splitpath(target_path)
+        dirs = context.remote_tree.list_dir(parent_dir)
+        dir_names = map(lambda x:'%s/' % util.unescape_arg(x['server_filename'].encode('utf-8')), dirs)
+        return filter(lambda x:len(name_prefix)==0 or x.startswith(name_prefix), dir_names)
