ff87b0d030b7e9be06701109b053434e99141d83
deadblue <9deadblue@gmail.com>
2014-09-29 16:26:57 +0800

    增加创建离线任务时自动洗种子的功能，稍后添加还原文件名功能

--- baidupan/command/dl.py
+++ baidupan/command/dl.py
@@ -7,9 +7,12 @@ Created on 2014/08/19
 
 import json
 import os
+import logging
 
-from baidupan import context
+from baidupan import context, bt
 from baidupan.command import Command
+
+_logger = logging.getLogger('dl')
 
 class CloudDownloadCommand(Command):
     def __init__(self):
@@ -19,19 +22,22 @@ class CloudDownloadCommand(Command):
         if len(args) == 0:
             print 'nothing to download'
         else:
-            self._download(args[0])
-    def _download(self, source):
+            # 判断是否需要混淆名称
+            mask = '--mask' in args
+            self._download(args[-1], mask)
+
+    def _download(self, source, mask=False):
         task_id = None
         if source.startswith('http:') or source.startswith('https:'):
             task_id = self._download_http(source)
-        # elif source.startswith('ftp:'):
-        #     task_id = self._download_ftp(source)
         elif source.startswith('ed2k:'):
             task_id = self._download_ed2k(source)
+        elif source.endswith('.torrent'):
+            task_id = self._download_torrent(source, mask)
+        # elif source.startswith('ftp:'):
+        #     task_id = self._download_ftp(source)
         # elif source.startswith('magnet:'):
         #     task_id = self._download_magnet(source)
-        elif source.endswith('.torrent'):
-            task_id = self._download_torrent(source)
         else:
             print 'unsupported source!'
         if task_id:
@@ -53,23 +59,34 @@ class CloudDownloadCommand(Command):
         result = context.client.cloud_dl_add_ed2k_task(link, context.get_rwd())
         task_id = result['task_id'] if result.has_key('task_id') else None
         return task_id
-    def _download_torrent(self, torrent_file):
+    def _download_torrent(self, torrent_file, mask=False):
+        save_path = context.get_rwd()
         source_path = None
-        # 从网盘上查找种子文件
-        file_list = context.remote_tree.list_file(context.get_rwd())
-        for file_obj in file_list:
-            if file_obj['server_filename'].encode('utf-8') == torrent_file:
-                source_path = file_obj['path']
-                break
-        # 若网盘上不存在则从本地查找
-        if source_path is None:
-            torrent_file = os.path.join(context.get_lwd(), torrent_file)
-            if os.path.exists(torrent_file):
-                # 若本地存在，则上传到网盘
+        # 查找种子文件
+        torrent_file = os.path.join(context.get_lwd(), torrent_file)
+        if os.path.exists(torrent_file):
+            if not mask:
+                # 上传种子文件
                 print 'upload torrent file: %s ...' % torrent_file
                 result = context.client.upload(context.get_rwd(), torrent_file)
                 source_path = result['path']
-        if source_path is None:
+            else:
+                print 'mask torrent file ...'
+                # 混淆bt文件
+                torrent_data, name_dict = bt.mask_file(torrent_file)
+                # 创建一层目录，作为下载存储目录
+                save_path = '%s%s/' % (save_path, name_dict['prefix'])
+                _logger.debug('save path: %s' % save_path)
+                context.client.create_dir(save_path)
+                # 上传混淆后的种子文件
+                print 'upload masked torrent ...'
+                result = context.client.upload_data(save_path, 'tmp.torrent', torrent_data)
+                source_path = result['path']
+                # 上传文件名字典
+                print 'upload name dictionary ...'
+                dict_data = json.dumps(name_dict)
+                context.client.upload_data(save_path, 'dictionary.txt', dict_data)
+        else:
             print 'no such torrent file!'
             return None
         # 获取种子文件信息
@@ -86,7 +103,7 @@ class CloudDownloadCommand(Command):
             selected_idx = map(lambda x:str(x+1), xrange(0, bt_info['file_count']))
             selected_idx = ','.join(selected_idx)
             # 创建离线下载任务
-            result = context.client.cloud_dl_add_bt_task(source_path, selected_idx, bt_info['sha1'], context.get_rwd())
+            result = context.client.cloud_dl_add_bt_task(source_path, selected_idx, bt_info['sha1'], save_path)
             task_id = result['task_id'] if result.has_key('task_id') else None
         # 删除种子文件
         print 'delete torrent file ...'
