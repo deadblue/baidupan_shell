080b6077210fb414ad41343a446cf781510e3bd6
deadblue <9deadblue@gmail.com>
2014-10-10 11:29:17 +0800

    实现下载目录功能

--- baidupan/command/pull.py
+++ baidupan/command/pull.py
@@ -5,19 +5,17 @@ Created on 2014/07/08
 @author: deadblue
 '''
 
-from baidupan import context, config
-from baidupan.command import Command, InvalidArgumentException, \
-    CommandExecuteException
 import os
 
-class NoSuchRemoteFileException(CommandExecuteException):
-    pass
+from baidupan import context, config
+from baidupan.command import Command
 
 class PullCommand(Command):
     def __init__(self):
         Command.__init__(self, 'pull', True)
 
     def execute(self, args):
+        save_path = context.get_lwd()
         for file_id in args:
             # 获取文件信息
             file_obj = context.remote_tree.get_file_by_id(file_id)
@@ -25,14 +23,15 @@ class PullCommand(Command):
                 print 'no such file: %s' % file_id
                 continue
             if file_obj['isdir'] == 0:
-                self._download_file(file_obj)
+                print 'pull file: %s ...' % file_obj['server_filename']
+                self._download_file(file_obj, save_path)
             else:
-                self._download_dir(file_obj)
-    def _download_file(self, file_obj):
-        file_name = file_obj['server_filename']
-        print 'pull file: %s ...' % file_name
+                print 'pull dir: %s ...' % file_obj['server_filename']
+                self._download_dir(file_obj, save_path)
+    def _download_file(self, file_obj, save_path):
         # 获取保存路径
-        save_path = os.path.join(context.get_lwd(), file_name)
+        save_path = os.path.join(save_path, file_obj['server_filename'])
+        # 下载请求
         download_req = context.client.get_download_request(file_obj['fs_id'])
         # 调用用户配置的下载器进行下载
         dler = config.get_downloader()
@@ -46,10 +45,19 @@ class PullCommand(Command):
             # 默认情况下使用curl
             from baidupan.downloader import curl
             curl.download(download_req, save_path)
-    def _download_dir(self, dir_obj):
-        raise Exception('unimplement!')
+    def _download_dir(self, dir_obj, save_path):
+        # 在本地创建目录
+        save_path = os.path.join(save_path, dir_obj['server_filename'])
+        os.makedirs(save_path)
+        # 遍历子文件，开始下载
+        sub_objs = context.remote_tree.list(parent_dir=dir_obj['path'], force_fetch=True)
+        for sub_obj in sub_objs:
+            if sub_obj['isdir'] != 0:
+                self._download_dir(sub_obj, save_path)
+            else:
+                self._download_file(sub_obj, save_path)
 
     def get_completer_words(self, prefix):
-        file_list = context.remote_tree.list_file(parent_dir=context.get_rwd())
+        file_list = context.remote_tree.list(parent_dir=context.get_rwd(), force_fetch=False)
         file_ids = map(lambda x:'%d ' % x['fs_id'], file_list)
         return filter(lambda x:len(prefix) == 0 or x.startswith(prefix), file_ids)
